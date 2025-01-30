5a054a45876c7983cf7b623c137630923bafa8b3
Aystb <3185269857@qq.com>
2025-01-30 16:53:35 +0800

    first

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view >
+		this is LUP
+		<u-icon name="map" size="24" ></u-icon>
+		<div>{{ testStore.item }}</div>
+	</view>
+</template>
+
+<script setup>
+import {useTestStore} from '../../store/testStore'
+		const testStore = useTestStore()
+</script>
+
+<style>
+	
+
+	
+</style>
