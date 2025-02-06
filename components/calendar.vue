a7f25a4ae694280bf85d3aec39be0404d928d8a0
JP Gao <Carpenter_G@163.com>
2025-02-06 12:41:22 +0800

    增加我的日程弹窗

--- components/calendar.vue
+++ components/calendar.vue
@@ -1,16 +1,32 @@
 <template>
-    <view>
-        this is calendar
-       <reserveCalendar></reserveCalendar>
-    </view>
-    
-    </template>
-    
-    <script setup>
-    import reserveCalendar from './reserve-calendar.vue';
-    
-    </script>
-    
-    <style scopd>
-    
-    </style>
+  <view>
+    <!-- this is calendar -->
+    <reserveCalendar
+      @showMyduty="show"
+      @showAddDuty="showAddDuty"
+    ></reserveCalendar>
+    <myDuty @close="close" :visible="isModalVisible"></myDuty>
+    <!-- <addDuty></addDuty> -->
+  </view>
+</template>
+
+<script setup>
+import reserveCalendar from './reserve-calendar.vue';
+import myDuty from './myDuty.vue';
+import addDuty from './addDuty.vue';
+import { ref } from 'vue';
+
+const isModalVisible = ref(false);
+const isAddDutyVisible = ref(false);
+
+// 显示添加我的日程弹窗
+function show() {
+  isModalVisible.value = true;
+}
+// 关闭我的日程弹窗
+function close() {
+  isModalVisible.value = false;
+}
+</script>
+
+<style scoped></style>
