b0dc07bdb8e7216ece257756f87f38af6dd158c0
Aystb <3185269857@qq.com>
2025-02-08 08:33:10 +0800

    feat/增加gitignore文件

--- components/talkMode.vue
+++ components/talkMode.vue
@@ -2,14 +2,16 @@
 <template>
     <view  class="flex-y">
         <button @click="visible=!visible">{{ requestModeStore.model }}</button>
-        <view v-if="visible" class="flex-y">
+        <view v-if="visible" class="flex-y hover_gray">
       
-       <view>{{ model }}
-        
+       <view>模型:{{ model }}
+
+
        </view>
-       <view>{{ max_tokens }}</view>
-       <view>{{ temperature }}</view>
-       <view>{{ stream }}</view>
+
+       <view class="flex-x">最大生成token数量{{ max_tokens }}</view>
+       <view class="flex-x">生成随机度{{ temperature }}</view>
+       <view class="flex-x">是否启用流式请求:{{ stream }}</view>
     </view>
 </view>
     </template>
@@ -27,8 +29,11 @@ const requestModeStore = useRequestModeStore()
 const max_tokens=ref(1600)
 const temperature = ref(0.5)  //介于 0 和 2 之间。较高的值（如 0.8）将使输出更加随机，而较低的值（如 0.2）将使输出更加集中和确定。
 const stream = ref(false)   
+const models = ref(["gpt-4o","gpt-3.5-turbo"])
     </script>
     
     <style scopd>
-    
+    .hover_gray{
+        background-color: ;
+    }
     </style>
