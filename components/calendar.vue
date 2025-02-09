7c9b8775869aec3a09c28afda6fdfcbb3b4ef2d1
Misay1125 <14862208+misay1125@user.noreply.gitee.com>
2025-02-09 18:20:25 +0800

    2.9

--- components/calendar.vue
+++ components/calendar.vue
@@ -1,23 +1,25 @@
 <template>
   <view>
-	<!-- 切换日历 -->
-    <reserveCalendar
-      @showMyDuty="showMyDuty()"
-    ></reserveCalendar>
-	
-	<!-- 我的日程 -->
-    <myDuty @close="close()" @showAddDuty="showAddDuty()" :visible="isMyDutyVisible"></myDuty>
-	
-	<!-- 添加日程 -->
+    <!-- 切换日历 -->
+    <reserveCalendar @showMyDuty="showMyDuty()"></reserveCalendar>
+
+    <!-- 我的日程 -->
+    <myDuty
+      @close="close()"
+      @showAddDuty="showAddDuty()"
+      :visible="isMyDutyVisible"
+    ></myDuty>
+
+    <!-- 添加日程 -->
     <addDuty @closeModal="closeAddDuty()" :visible="isAddDutyVisible"></addDuty>
   </view>
 </template>
 
 <script setup>
-import reserveCalendar from './reserve-calendar.vue';
-import myDuty from './myDuty.vue';
-import addDuty from './addDuty.vue';
-import { ref } from 'vue';
+import reserveCalendar from "./reserve-calendar.vue";
+import myDuty from "./myDuty.vue";
+import addDuty from "./addDuty.vue";
+import { ref } from "vue";
 
 // 我的日程
 const isMyDutyVisible = ref(false);
@@ -42,8 +44,6 @@ function showAddDuty() {
 function closeAddDuty() {
   isAddDutyVisible.value = false;
 }
-
-
 </script>
 
 <style scoped></style>
--- components/myDuty.vue
+++ components/myDuty.vue
@@ -1,97 +1,92 @@
-<template>	
-	<view>
-		<view v-if="props.visible" class="modalOverlay" @click="closeMyDuty()">
-			<view class="z-9">
-				<view class="ft-16 mb-10 ml-10">这是当前日期</view>
-				<!-- 白色盒子 -->
-				<view class="dutyContainer" @click.stop>
-					
-					<!-- 头部：我的日程 -->
-					<view class="myDuty-font">
-						<view class="font fw-600">我的日程</view>
-						<view class="ft-18 fw-600">——my duty——</view>
-					</view>
-					
-					<!-- 中部：添加的代办事项 -->
-					<view class="duties">
-						
-					</view>
-					
-					<!-- 尾部：加号按钮 -->
-					<button class="addDuty_btn" @click="showAddDuty()">
-						<image src="/static/addDuty_btn.png"></image>
-					</button>
-				</view>
-			</view>
-		</view>
-	</view>
+<template>
+  <view>
+    <view v-if="props.visible" class="modalOverlay" @click="closeMyDuty()">
+      <view class="z-9">
+        <view class="ft-16 mb-10 ml-10">{{}}年{{}}月{{}}日</view>
+        <!-- 白色盒子 -->
+        <view class="dutyContainer" @click.stop>
+          <!-- 头部：我的日程 -->
+          <view class="myDuty-font">
+            <view class="font fw-600">我的日程</view>
+            <view class="ft-18 fw-600">——my duty——</view>
+          </view>
+
+          <!-- 中部：添加的代办事项 -->
+          <view class="duties"> </view>
+
+          <!-- 尾部：加号按钮 -->
+          <button class="addDuty_btn" @click="showAddDuty()">
+            <image src="/static/addDuty_btn.png"></image>
+          </button>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script setup>
-	import { defineProps, defineEmits } from "vue";
-	
-	const props =defineProps({
-		visible: Boolean,
-		default:false
-	})
-	// 关闭我的日程
-	
-	const emit=defineEmits(['close', 'showAddDuty'])
+import { defineProps, defineEmits } from "vue";
 
-	function closeMyDuty() {
-		emit("close")
-		console.log("关闭我的日程页面")
-	}
-	
-	// 显示添加日程界面
-	function showAddDuty () {
-		emit("showAddDuty")
-		console.log("显示添加日程页面")
-	}
-	
+const props = defineProps({
+  visible: Boolean,
+  default: false,
+});
+// 关闭我的日程
+
+const emit = defineEmits(["close", "showAddDuty"]);
+
+function closeMyDuty() {
+  emit("close");
+  console.log("关闭我的日程页面");
+}
+
+// 显示添加日程界面
+function showAddDuty() {
+  emit("showAddDuty");
+  console.log("显示添加日程页面");
+}
 </script>
 
 <style scoped>
-	.modalOverlay {
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	  position: fixed;
-	  width: 100%;
-	  height: 100%;
-	  background-color: rgba(227, 226, 226, 0.5);
-	  z-index: 8000;
-	}
+.modalOverlay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(227, 226, 226, 0.5);
+  z-index: 8000;
+}
 
-	.dutyContainer {
-	  background-color: #fff;
-	  padding: 20px;
-	  border-radius: 5px;
-	  position: relative;
-	  width: 300px;
-	  height: 500px;
-	  border: 1px solid #DEB0FF9C;
-	  border-radius:36px
-	}
-	.font{
-		font-size: 36px;
-	}
-	.addDuty_btn {
-		height: 60px;
-		width:60px;
-		border-radius: 50%;
-		background-color: transparent;
-		/* border: none; */
-		box-shadow: none;
-		padding: 0;
-		margin: 0;
-		position: absolute;
-		bottom: 20px;
-		right: 20px;
-	}
-	.addDuty_btn image {
-		height: 60px;
-		width:60px;
-	}
+.dutyContainer {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  position: relative;
+  width: 300px;
+  height: 500px;
+  border: 1px solid #deb0ff9c;
+  border-radius: 36px;
+}
+.font {
+  font-size: 36px;
+}
+.addDuty_btn {
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  background-color: transparent;
+  /* border: none; */
+  box-shadow: none;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+.addDuty_btn image {
+  height: 60px;
+  width: 60px;
+}
 </style>
-
